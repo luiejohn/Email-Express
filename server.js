a222cf63ed8af75d06f5d3a9a06ef889d6433058
Luie <luiejohn.24@gmail.com>
2019-08-14 16:33:31 +0800

    scripts heroku setup and basic react app

--- server.js
+++ server.js
@@ -5,7 +5,19 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.send({'hi': 'asdasds'})
-})
+});
+
+
+if ( process.env.NODE_ENV === 'production' ) {
+    //Serve React App production assets
+    app.use(express.static('client/build'));
+
+    //server index.html for unknown server routes
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 
 const PORT = process.env.PORT || 5000;
